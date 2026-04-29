a26784b327c6d92f76fef6df989385e549aa45f4
jmromeros12-web <jmromeros12@educarex.es>
2026-04-29 10:08:45 +0200

    ko

---
+++ kodekata.js
@@ -0,0 +1,16 @@
+
+
+//let result = function getCopyright(name, year) {
+    //let copyright = formatter(name, year);
+    //return copyright;
+//};
+
+//let formatter = function(name, year) {
+    //return name + " | " + year;
+//}
+
+//console.log(result("Armando", 2026, formatter));
+
+(function(){
+    console.log("ok");
+}());
